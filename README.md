e68566638cbf058c856def94212e78c1bdc6e2c9
Aneesh Durg <durg2@illinois.edu>
2017-02-16 15:14:04 -0600

    Some fixes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # brainfuck
+
+Usage: 
+
+*  ghc bf-interpreter.hs
+*  ./bf-interpreter [filename]
+
+If a filename isn't specified, the program will be read from stdin
--- bf-interpreter.hs
+++ bf-interpreter.hs
@@ -1,8 +1,9 @@
 import Data.HashMap.Strict as H (HashMap, empty, fromList, insert, lookup, union)
 import System.Environment
+import System.IO
 
-type OpType = [Char] -> Int -> IO ([Char], Int)
-type LoopType = [Char] -> Int -> [Char] -> IO ([Char], Int)
+type OpType = [Char] -> Int -> Handle -> IO ([Char], Int)
+type LoopType = [Char] -> Int -> [Char] -> Handle -> IO ([Char], Int)
 
 ops :: H.HashMap Char OpType
 ops = H.fromList [ ('>', incP)
@@ -16,55 +17,51 @@ ops = H.fromList [ ('>', incP)
                  ]
 
 incP :: OpType
-incP mem idx = do
-                 putStrLn "i"
+incP mem idx _ = do
                  return (mem, idx+1)
 
 decP :: OpType
-decP mem idx = do
-                 putStrLn "d"
+decP mem idx _ = do
                  return (mem, idx-1)
 
 inc :: OpType
-inc mem idx = do
-                 putStrLn "I"
+inc mem idx _ = do
                  return (take idx mem ++ succ (mem!!idx):[] ++ drop (idx+1) mem, idx)
 dec :: OpType
-dec mem idx = do
-                 putStrLn "I"
+dec mem idx _ = do
                  return (take idx mem ++ pred (mem!!idx):[] ++ drop (idx+1) mem, idx)
 
 putc :: OpType
-putc mem idx = do
-                 putStrLn $ "p: "++(mem!!idx):[]
+putc mem idx _ = do
+                 putStr $ (mem!!idx):[]
                  return (mem, idx)
 getc :: OpType
-getc mem idx = do
-                 c <- getChar
+getc mem idx _ = do
+                 c <- hGetChar stdin
                  return (take idx mem ++ c:[] ++ drop (idx+1) mem, idx)
 
-genLoop :: String -> IO String
-genLoop x = do
-  c <- getChar
+genLoop :: String -> Handle -> IO String
+genLoop x h = do
+  c <- hGetChar h
   case c of 
     '['  -> do
-              l <- genLoop "["
-              genLoop $ (reverse l) ++ x
+              l <- genLoop "[" h
+              genLoop ((reverse l) ++ x) h
     ']' -> return $ reverse $ c:x
     _   -> case H.lookup c ops of 
-      Just _ -> genLoop $ c:x
-      Nothing -> genLoop x
+      Just _ -> genLoop (c:x) h
+      Nothing -> genLoop x h
 
 regenLoop :: String -> String -> (String, String)
 regenLoop x (l:ls) =
   case l of 
-    '['  -> let (sub, rest) = regenLoop "[" $ tail ls
+    '['  -> let (sub, rest) = regenLoop "[" ls
             in  regenLoop ((reverse sub) ++ x) rest
     ']' -> (reverse $ l:x, ls)
     _   -> regenLoop (l:x) ls
 
 loopEval :: LoopType
-loopEval mem idx l = do
+loopEval mem idx l h = do
                        loopEvalHelp mem idx [] l
                        where
                          loopEvalHelp m i prev []       = return (m, i)  
@@ -81,34 +78,34 @@ loopEval mem idx l = do
 
                          loopEvalHelp m i prev (x:xs)   = do 
                                                             let Just op = H.lookup x ops
-                                                            (m', i') <- op m i
+                                                            (m', i') <- op m i h
                                                             loopEvalHelp m' i' (x:prev) xs
                                                       
 
 
-jmpf :: OpType
-jmpf mem idx = do
-                 l <- genLoop "["
-                 putStrLn l
-                 loopEval mem idx l
+jmpf :: OpType 
+jmpf mem idx h = do
+                 l <- genLoop "[" h
+                 loopEval mem idx l h
 
 
 main = do
   args <- getArgs
-  let n = read (head args) :: Int
-  repl (concat $ repeat ['\0']) 0
+  if (length args) > 0
+    then do
+      h <- openFile (head args) ReadMode 
+      repl (concat $ repeat ['\0']) 0 h
+    else repl (concat $ repeat ['\0']) 0 stdin
 
-repl mem idx = do
-  print (take (idx+1) mem, idx)
-  c <- getChar
+repl mem idx h = do
+  --print (take (idx+1) mem, idx)
+  c <- hGetChar h
   if c/='\n' then do
     case H.lookup c ops of
       Just op -> do
-        (mem', idx') <- op mem idx
-        repl mem' idx' 
-      _       -> do
-        putStrLn $ "Error op "++(c:[])++" not found!"
-        repl mem idx
+        (mem', idx') <- op mem idx h 
+        repl mem' idx' h
+      _       -> repl mem idx h
   else
-    repl mem idx
+    repl mem idx h
   
